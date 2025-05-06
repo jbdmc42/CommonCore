bb982308f9ddd34f5093573bf874be3d67cac698
Joao Barbosa De Meneses Correia <joaobarb@c1r12s8.42porto.com>
2025-05-06 13:50:59 +0100

    updated get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaobarb <joaobarb@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 11:53:22 by joaobarb          #+#    #+#             */
+/*   Updated: 2025/05/06 13:29:25 by joaobarb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE
+
+
+// Functions prototypes
+
+// get_next_line
+
+char	*get_next_line(int fd);
+
+#endif
