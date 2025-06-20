af4c3fb34d1b043478712d17248299e9df42f0f3
Joao Barbosa De Meneses Correia <joaobarb@c1r11s7.42porto.com>
2025-06-20 11:31:27 +0100

    updated get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaobarb <joaobarb@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 11:53:22 by joaobarb          #+#    #+#             */
+/*   Updated: 2025/06/17 13:44:30 by joaobarb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*gnl_strjoin(char *s1, char *s2);
+char	*gnl_strchr(char *s, int c);
+char	*gnl_strdup(char *s);
+char	*gnl_substr(char *s, int start, int len);
+int		gnl_strlen(char *s);
+
+#endif
